0e8fa6034d5f8f1e2996e2d42e462f574047b833
Zachary Foutz <zacharyfoutz@gmail.com>
2023-01-20 14:33:17 -0800

    cleanup
    * cleaned up squid

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,18 +1,20 @@
 extends Squid
 
+var speed = 25
+
 func _ready():
 	friction.x = 0.92
-	friction.y = 0.92
+	friction.y = 0.90
 
-func _physics_process(delta):
+func _tick(delta):
+	velocity.y += 98
+	
 	if Input.is_action_pressed("ui_left"):
-		velocity.x += -1
-
+		velocity.x -= speed
+		
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		velocity.x += speed
 
 	if Input.is_action_pressed("ui_up"):
-		velocity.y += -1
-
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		if is_on_floor():
+			velocity.y -= 5000
--- Scripts/Squid.gd
+++ Scripts/Squid.gd
@@ -1,12 +1,21 @@
-extends Node2D
+extends KinematicBody2D
 class_name Squid
 
-var velocity = Vector2(0, 0)
-var friction = Vector2(0, 0)
-
+export var velocity = Vector2(0, 0)
+export var friction = Vector2(0, 0)
 export (bool) var alive = true
+export (Image) var sprite = null
+onready var image = get_node("Image")
+
+func _ready():
+	if sprite != null:
+		image.set_texture(sprite)
+
+func _tick(delta):
+	pass
 
 func _process(delta):
-	
-	translate(velocity)
-	velocity *= friction
+	if alive:
+		_tick(delta)
+		move_and_slide(velocity, Vector2.UP)
+		velocity *= friction
